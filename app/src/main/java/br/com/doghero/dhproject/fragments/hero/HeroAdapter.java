6fabee4ccb9aab21bb6b8773d09345b7ccaffb9e
MaxTgr <raranmariano@hotmail.com>
2018-10-28 09:19:06 -0300

    favorite button toggle WIP

--- app/src/main/java/br/com/doghero/dhproject/fragments/hero/HeroAdapter.java
+++ app/src/main/java/br/com/doghero/dhproject/fragments/hero/HeroAdapter.java
@@ -2,9 +2,11 @@ package br.com.doghero.dhproject.fragments.hero;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,6 +29,7 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.ViewHolder> {
 
         public TextView name, price, neighborhood;
         public ImageView photo, superhero;
+        public ImageButton btnFavorite;
 
         public ViewHolder(View view) {
             super(view);
@@ -35,6 +38,8 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.ViewHolder> {
             neighborhood = view.findViewById(R.id.hero_neighborhood);
             photo = view.findViewById(R.id.hero_photo);
             superhero = view.findViewById(R.id.hero_is_superhero);
+
+            btnFavorite = view.findViewById(R.id.favorite_button);
         }
     }
 
@@ -49,9 +54,10 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         Hero hero = mHeroes.get(position);
 
+        // Setting text
         holder.name.setText(hero.getFirstName());
         holder.neighborhood.setText(hero.getAddressNeighborhood());
         String heroPrice = Integer.toString(hero.getPrice());
@@ -59,10 +65,31 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.ViewHolder> {
 
         // TODO: ask - favorite button should not appear on recent?
         // TODO: change placeholder img
+        // Image loading
         ImageHelper.loadImage(ctx, hero.getImageUrl(), R.mipmap.ic_launcher, holder.photo);
         // show or hide superhero icon
         int superheroCheck = hero.getIsSuperHero()? View.VISIBLE : View.GONE;
         holder.superhero.setVisibility(superheroCheck);
+
+        // OnClickHandlers
+        holder.btnFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int drawable;
+                String tag = (String) holder.btnFavorite.getTag();
+
+                // TODO: finish tag toggle
+                if(tag.equals("true")){
+                    drawable = R.drawable.icon_like_border_vector_gray_battleship;
+                    tag = "false";
+                }else{
+                    drawable = R.drawable.icon_like_filled_vector_red;
+                    tag = "true";
+                }
+                Log.e("holder", holder.btnFavorite.getTag().toString());
+                holder.btnFavorite.setImageResource(drawable);
+            }
+        });
     }
 
     @Override
